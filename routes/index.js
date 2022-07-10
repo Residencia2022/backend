fbeebcd80596372dade4cc2cdf4c0e2214283ec5
Jesus Alvarado Reyes <jesusar.dev@gmail.com>
2022-07-09 19:48:38 -0500

    add: files for calendar and interns

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import calendarRouter from './calendar.router.js';
+import internsRouter from './interns.router.js';
 import positionsRouter from './positions.router.js';
 import productsRouter from './products.router.js';
 import schedulesRouter from './schedules.router.js';
@@ -9,6 +11,8 @@ const loginRouter = Router();
 loginRouter.use('/sessions', sessionsRouter);
 
 const apiRouter = Router();
+apiRouter.use('/calendar', calendarRouter);
+apiRouter.use('/interns', internsRouter);
 apiRouter.use('/positions', positionsRouter);
 apiRouter.use('/products', productsRouter);
 apiRouter.use('/schedules', schedulesRouter);
