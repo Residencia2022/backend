7e61060bfa1ee2a9549e9c491e3f27f081ad9aa5
Jesus Alvarado Reyes <jesusar.dev@gmail.com>
2022-07-11 21:03:53 -0500

    add: controller and router to create intern

--- schemas/interns.schema.js
+++ schemas/interns.schema.js
@@ -1,1 +1,30 @@
 import Joi from 'joi';
+
+const ID_INTERN = Joi.number().integer().min(0);
+const FIRST_NAME = Joi.string().min(3).max(100);
+const LAST_NAME = Joi.string().min(3).max(150);
+const DEGREE = Joi.string().min(3).max(100);
+const INTEREST = Joi.string().min(2).max(1000);
+const MOTIVATION = Joi.string().min(2).max(1000);
+const EXPERIENCE = Joi.string().min(2).max(1000);
+const ENGLISH_LEVEL = Joi.string().length(2);
+const ID_POSITION = Joi.number().integer().min(1).max(3);
+const PHONE_NUMBER = Joi.string().min(10).max(13);
+const EMAIL = Joi.string().max(100).email();
+const CV = Joi.string().min(5).max(100);
+
+const createInternSchema = Joi.object({
+  FIRST_NAME: FIRST_NAME.required(),
+  LAST_NAME: LAST_NAME.required(),
+  DEGREE: DEGREE.required(),
+  INTEREST: INTEREST.required(),
+  MOTIVATION: MOTIVATION.required(),
+  EXPERIENCE: EXPERIENCE.required(),
+  ENGLISH_LEVEL: ENGLISH_LEVEL.required(),
+  ID_POSITION: ID_POSITION.required(),
+  PHONE_NUMBER: PHONE_NUMBER.required(),
+  EMAIL: EMAIL.required(),
+  CV: CV.required(),
+});
+
+export { createInternSchema };
