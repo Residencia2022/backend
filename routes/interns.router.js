160bae2fc4f74f5ca1dcd34b9eab41bdd6945fe4
CesarSebastianMP <sebas123mera@gmail.com>
2022-07-12 20:48:00 -0500

    Creacion del get para interns id

--- routes/interns.router.js
+++ routes/interns.router.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import validatorHandler from '../middlewares/validator.handler.js';
-import { createInternSchema, deleteInternSchema } from '../schemas/interns.schema.js';
-import { createIntern, deleteIntern } from '../controllers/interns.controller.js';
+import { createInternSchema, deleteInternSchema, getInternSchema } from '../schemas/interns.schema.js';
+import { createIntern, deleteIntern, getIntern } from '../controllers/interns.controller.js';
 
 
 const internsRouter = Router();
@@ -28,6 +28,20 @@ internsRouter.delete(
       const id = req.params.ID_INTERN;
       const deletedIntern = await deleteIntern(id);
       res.json({ data: deletedIntern });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+internsRouter.get(
+  '/:ID_INTERN',
+  validatorHandler(getInternSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const id = req.params.ID_INTERN;
+      const getedIntern = await getIntern(id);
+      res.json({ data: getedIntern });
     } catch (error) {
       next(error);
     }
