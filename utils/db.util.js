6edfba31a7412a4cfd9bc90351678791d9f7b183
Jesus Alvarado Reyes <jesusar.dev@gmail.com>
2022-10-16 16:51:59 -0500

    Restore SSL configuration

--- utils/db.util.js
+++ utils/db.util.js
@@ -1,7 +1,7 @@
 import { createConnection } from 'mysql2/promise';
-// import { readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 
-// const DB_CA = [readFileSync('./DigiCertGlobalRootCA.crt.pem', 'utf8')];
+const DB_CA = [readFileSync('./DigiCertGlobalRootCA.crt.pem', 'utf8')];
 
 const db = async (sql, values) => {
   const connection = await createConnection({
@@ -10,10 +10,10 @@ const db = async (sql, values) => {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    // ssl: {
-    //   rejectUnauthorized: true,
-    //   ca: DB_CA,
-    // },
+    ssl: {
+      rejectUnauthorized: true,
+      ca: DB_CA,
+    },
   });
   const [results] = await connection.execute(sql, values);
   await connection.end();
